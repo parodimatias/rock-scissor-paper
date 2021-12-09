67db43b643d56f44545b14d59857334e6977865d
MatiP <matias.parodi@outlook.com>
2021-12-09 10:47:42 -0300

    first commit

---
+++ server/index.ts
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static("dist"));
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+app.get("/", (req, res) => {
+  res.json("hola");
+});
